aa72544ff80ab560be2eced4e5a19ead5d072a04
piercebergstein <pierce.bergstein@berkeley.edu>
2026-07-09 17:18:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,6 @@ if run:
                     "Title": r.title or r.query,
                     "Release Date": r.release_date or "",
                     "Domestic Box Office": r.domestic_total or "",
-                    "Status": r.status,
                     "BOM URL": r.url or "",
                 }
             )
